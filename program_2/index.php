72126a1d2e695dee3ca6e809fca2d9269ca8c2b0
Mounika Madireddy <mounicareddy555@gmail.com>
2016-07-19 02:43:57 -0500

    Rename register.php to index.php

--- program_2/index.php
+++ program_2/index.php
@@ -188,7 +188,7 @@ if( $fname && $lname && $uname && $email && $pass1 && $pass2){
 	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">Ã—</button>
 				<h4 class="modal-title" id="myModalLabel">Terms & Conditions</h4>
 			</div>
 			<div class="modal-body">
